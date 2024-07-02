51ac659be22b5d598e485e487dcca07d51e434f5
Ritik <113279473+ritik-3@users.noreply.github.com>
2024-07-02 16:18:14 +0530

    Delete Basic queries and database creation directory

--- Basic queries and database creation/Day 2 (new table).sql
+++
@@ -1,9 +0,0 @@
- use cybrom;
- show databases;
- create table stu(s_id int,s_name varchar(20),age tinyint,fees decimal(10,2),d_o_b date);
- desc stu;
- rename table stu to stu_1;
- desc stu_1;
- drop table stu_1;
- drop database cybrom;
- 
